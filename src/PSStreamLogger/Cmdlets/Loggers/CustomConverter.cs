bd8c31562810ae5180cb1e4e91924f33912b8d0e
Roman Andres <roman.andres@netree.ch>
2023-04-13 23:11:06 +0200

    Implement app insights test

---
+++ src/PSStreamLogger/Cmdlets/Loggers/CustomConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ApplicationInsights.Channel;
+using Serilog.Events;
+using Serilog.Sinks.ApplicationInsights.TelemetryConverters;
+
+namespace PSStreamLoggerModule
+{
+    internal class CustomConverter : TraceTelemetryConverter
+    {
+        private readonly string? scriptName;
+
+        public CustomConverter(string? scriptName)
+        {
+            this.scriptName = scriptName;
+        }
+
+        public override IEnumerable<ITelemetry> Convert(LogEvent logEvent, IFormatProvider formatProvider)
+        {
+            foreach (ITelemetry telemetry in base.Convert(logEvent, formatProvider))
+            {
+                if (!string.IsNullOrWhiteSpace(scriptName))
+                {
+                    telemetry.Context.GlobalProperties.Add("ScriptName", scriptName);
+                }
+
+                yield return telemetry;
+            }
+        }
+    }
+}
---
+++ src/PSStreamLogger/Cmdlets/Loggers/NewAzureApplicationInsightsLogger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.ApplicationInsights.TelemetryConverters;
+using Serilog.Templates;
+
+namespace PSStreamLoggerModule
+{
+    [Cmdlet(VerbsCommon.New, "AzureApplicationInsightsLogger")]
+    public class NewAzureApplicationInsightsLogger : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string? ConnectionString { get; set; }
+
+        [Parameter()]
+        public string? ScriptName { get; set; }
+
+        [Parameter()]
+        public string? FilterIncludeOnlyExpression { get; set; }
+
+        [Parameter()]
+        public string? FilterExcludeExpression { get; set; }
+
+        [Parameter()]
+        public Serilog.Events.LogEventLevel MinimumLogLevel { get; set; } = Serilog.Events.LogEventLevel.Information;
+
+        protected override void EndProcessing()
+        {
+            var loggerConfiguration = new Serilog.LoggerConfiguration()
+            .MinimumLevel.Is(MinimumLogLevel)
+                .WriteTo.ApplicationInsights(
+                     connectionString: ConnectionString,
+                     telemetryConverter: new CustomConverter(ScriptName),
+                     restrictedToMinimumLevel: MinimumLogLevel)
+                .Enrich.FromLogContext();
+
+            if (FilterIncludeOnlyExpression is object)
+            {
+                loggerConfiguration = loggerConfiguration
+                    .Filter.ByIncludingOnly(FilterIncludeOnlyExpression);
+            }
+
+            if (FilterExcludeExpression is object)
+            {
+                loggerConfiguration = loggerConfiguration
+                    .Filter.ByExcluding(FilterExcludeExpression);
+            }
+
+            WriteObject(loggerConfiguration.CreateLogger());
+        }
+    }
+}
