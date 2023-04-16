6c931436ff777f1c03deae233415524e1c0d341f
Roman Andres <roman@ndres.dev>
2023-04-16 19:25:38 +0200

    Test XmlDoc2CmdDoc

--- src/PSStreamLogger/Cmdlets/Loggers/NewConsoleLogger.cs
+++ src/PSStreamLogger/Cmdlets/Loggers/NewConsoleLogger.cs
@@ -6,18 +6,38 @@ using Serilog.Templates;
 
 namespace PSStreamLoggerModule
 {
+    /// <summary>
+    /// <para type="synopsis">Creates a new Console logger.</para>
+    /// <para type="description">Console loggers log to the console.</para>
+    /// </summary>
     [Cmdlet(VerbsCommon.New, "ConsoleLogger")]
     public class NewConsoleLogger : PSCmdlet
     {
+        /// <summary>
+        /// <para type="description">This is part of the parameter description.</para>
+        /// <para type="description">This is also part of the parameter description.</para>
+        /// </summary>
         [Parameter()]
         public string ExpressionTemplate { get; set; } = Logger.DefaultExpressionTemplate;
 
+        /// <summary>
+        /// <para type="description">This is part of the parameter description.</para>
+        /// <para type="description">This is also part of the parameter description.</para>
+        /// </summary>
         [Parameter()]
         public string? FilterIncludeOnlyExpression { get; set; }
 
+        /// <summary>
+        /// <para type="description">This is part of the parameter description.</para>
+        /// <para type="description">This is also part of the parameter description.</para>
+        /// </summary>
         [Parameter()]
         public string? FilterExcludeExpression { get; set; }
 
+        /// <summary>
+        /// <para type="description">This is part of the parameter description.</para>
+        /// <para type="description">This is also part of the parameter description.</para>
+        /// </summary>
         [Parameter()]
         public Serilog.Events.LogEventLevel MinimumLogLevel { get; set; } = Logger.DefaultMinimumLogLevel;
 
